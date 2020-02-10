7f9455b73e4d01ec93fd233de0c15f03e8cf2667
martabtab <57708251+martabtab@users.noreply.github.com>
2020-02-10 11:12:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 function fetchNorrisJSON() {
-  // Feel free to download this HTML and edit it, to use another Pokemon ID
-  // const jokeId = 1;
   const url = `https://api.chucknorris.io/jokes/random/`;
-  //${jokeId}
   axios
     .get(url)
-    .then(response => response.data) // DIFFERENT FROM FETCH: response.data instead of response.json()
+    .then(response => response.data)
     .then(joke => {
       {
         console.log("data decoded from JSON:", joke);
 
-        // Build a block of HTML
         const jokeHtml = `
         <p><strong>${joke.value}</strong></p>
         <img src="${joke.icon_url}" />
